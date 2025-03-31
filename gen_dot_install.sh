3107bdb1d5e4c7461e56fbc56110f4a3b37c50d9
Samuel Hym <samuel@tarides.com>
2025-03-31 18:47:02 +0200

    Simplify `gen_dot_install.sh`

--- gen_dot_install.sh
+++ gen_dot_install.sh
@@ -5,26 +5,15 @@
 
 ARCH="$1"
 
-install_file() {
-  # install_file <src> [dest]
-  if [ -z "$2" ]; then
-    printf '  "%s"\n' "$1"
-  else
-    printf '  "%s" { "%s" }\n' "$1" "$2"
-  fi
-}
-
 main() {
-  printf '%s: [\n' lib_root
-  install_file _build/unikraft_"$ARCH".conf findlib.conf.d/unikraft_"$ARCH".conf
-  printf ']\n'
-
-  printf '%s: [\n' lib
-  # dummy packages
-  for pkg in threads is_unikraft; do
-    install_file _build/empty lib/$pkg/META
-  done
-  printf ']\n'
+  printf 'lib_root: [
+  "_build/unikraft_%s.conf" { "findlib.conf.d/unikraft_%s.conf" }
+]
+lib: [
+  "_build/empty" { "lib/threads/META" }
+  "_build/empty" { "lib/is_unikraft/META" }
+]
+' "$ARCH" "$ARCH"
 }
 
 main
